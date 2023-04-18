ac31357c3bcda48baa7dbda11f876b58b4a203bc
javibeol <javierbeol@hotmail.com>
2023-04-18 22:53:43 +0200

    Tutorial y pruebas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MallasComponent } from './mallas/mallas.component';
+import { MallaComponent } from './malla/malla.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, MallasComponent, MallaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
